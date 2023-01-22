3e0f5053eb37a3dd9d753425e4cea2512043c2b9
Ardian Maulana Effendi <ardian.effendi21@gmail.com>
2023-01-22 22:13:17 +0800

    revert back "deliberate error for testing" and add routes.go as a place to isolate the application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -43,27 +43,10 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	// Use the http.NewServeMux() function to initialize a new serveMux, then
-	// register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	// Create a file server which serves files out of the "./ui/static" directory
-	// Note that the path given to the http.Dir function is relative to the program
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	// Write messages using the tow new loggers, instead of the standard logger.
