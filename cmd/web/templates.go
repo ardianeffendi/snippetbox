f8df979d324dc0c49aa04739304a6de7c8dd04e6
Ardian Maulana Effendi <ardian.effendi21@gmail.com>
2023-01-28 22:12:50 +0800

    add newTemplateCache() function to cache the templates.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,10 +1,63 @@
 package main
 
-import "github.com/ardianeffendi/snippetbox/pkg/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/ardianeffendi/snippetbox/pkg/models"
+)
 
 // Define a templateData type to act as the holding structure for
 // any dynamic data that we want to pass to our HTML templates.
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	// Initialise a new map to act as the cache.
+	cache := map[string]*template.Template{}
+
+	// Use the filepath.Glob function to get a slice of all the filepaths with
+	// the extension '.page.tmpl'. This essentially gives us a slice of all the
+	// 'page' templates for the application.
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+	if err != nil {
+		return nil, err
+	}
+
+	// Loop through the pages one-by-one.
+	for _, page := range pages {
+		// Extract the file name (like 'home.page.tmpl') from the full file path
+		// and assign it to the new variable.
+		name := filepath.Base(page)
+
+		// Parse the page template file in to a template set.
+		ts, err := template.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		// Use the ParseGlob method to add any 'layout' templates to the
+		// template set (in our case, it's just the 'base' layout at the
+		// moment).
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmp"))
+		if err != nil {
+			return nil, err
+		}
+
+		// Use the ParseGlob method to add any 'partial' templates to the
+		// template set (in our case, it's just the 'base' layout at the
+		// moment).
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmp"))
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the template set to the cache, using the name of the page
+		// (like 'home.page.tmpl') as the key.
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
