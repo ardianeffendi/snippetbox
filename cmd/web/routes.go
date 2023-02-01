25e74f764bbde0bca79c8eee76b00cb0ad68ce83
Ardian Maulana Effendi <ardian.effendi21@gmail.com>
2023-02-01 19:20:11 +0800

    add a panic recover and composable middleware by justinas/alice

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,10 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // routes function returns a pointer to ServeMux which holds registered
 // routers of this application.
 func (app *application) routes() http.Handler {
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	// Use the http.NewServeMux() function to initialize a new serveMux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
@@ -22,6 +30,5 @@ func (app *application) routes() http.Handler {
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// Wrap the secureHeaders middleware with the logRequest middleware.
-	return app.logRequest(secureHeaders(mux))
+	return standardMiddleware.Then(mux)
 }
