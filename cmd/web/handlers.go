971ee0101661ac4bee41c780a212d9d5a4c1664e
Ardian Maulana Effendi <ardian.effendi21@gmail.com>
2023-02-08 18:39:41 +0800

    add better data validation - abstracted the functionality of forms and its error handling to its   own package which can be reused throughout the application - slight changes on create.page.tmpl following the new form package

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
+	"github.com/ardianeffendi/snippetbox/pkg/forms"
 	"github.com/ardianeffendi/snippetbox/pkg/models"
 )
 
@@ -48,7 +47,10 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "create.page.tmpl", nil)
+	app.render(w, r, "create.page.tmpl", &templateData{
+		// Pass a new empty forms.Form object to the template.
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
@@ -62,50 +64,23 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the r.PostForm.Get() method to retrieve the relevant data fields
-	// from the r.PostForm map.
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-	expires := r.PostForm.Get("expires")
+	// Create a new forms.Form struct containing the POSTed data from the
+	// form, then use the validation methods to check the content.
+	form := forms.New(r.PostForm)
+	form.Required("title", "content", "expires")
+	form.MaxLength("title", 100)
+	form.PermittedValues("expires", "365", "7", "1")
 
-	// Initialise a map to hold any validation errors.
-	errors := make(map[string]string)
-
-	// Check that the title field is not blank and is not more than 100 chars long.
-	// If it fails either of those checks, add a message to the errors map
-	// using the field name as the key.
-	if strings.TrimSpace(title) == "" {
-		errors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		errors["title"] = "This field is too long (maximum is 100 characters)"
-	}
-
-	// Check that the Content field isn't blank.
-	if strings.TrimSpace(content) == "" {
-		errors["content"] = "This field cannot be blank"
-	}
-
-	// Check the expires field isn't blank and matched one of the permitted
-	// values ("1", "7", or "365").
-	if strings.TrimSpace(expires) == "" {
-		errors["expires"] = "This field cannot be blank"
-	} else if expires != "365" && expires != "7" && expires != "1" {
-		errors["expires"] = "This field is invalid"
-	}
-
-	// If there are any errors, dump it in a plain text HTTP response and return
-	// from the handler.
-	if len(errors) > 0 {
+	// If the form isn't valid, redisplay the template passing in the
+	// form.Form object as the data.
+	if !form.Valid() {
 		app.render(w, r, "create.page.tmpl", &templateData{
-			FormErrors: errors,
-			FormData:   r.PostForm,
+			Form: form,
 		})
 		return
 	}
 
-	// Pass the data to the SnippetModel.Insert() method, receiving the ID
-	// of the new record back.
-	id, err := app.snippets.Insert(title, content, expires)
+	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
 	if err != nil {
 		app.serverError(w, err)
 		return
