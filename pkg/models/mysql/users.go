333139ae6a26124e944960113690d28d0f9e60af
Ardian Maulana Effendi <ardian.effendi21@gmail.com>
2023-02-13 00:27:32 +0800

    add user login page and authentication - it is now able to authenticate existing user in database - user can logout - NOTE: it is incomplete as there is no user authorization to limit the   access of other features in the web app

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -45,7 +45,28 @@ func (m *UserModel) Insert(name, email, password string) error {
 // Authenticate() method verifies whether a user exists with the provided
 // email address and password. This will return the relevant user ID if they do.
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	// Retrieve the id and hashed password associated with the given email.
+	// If no matching email exists, we return the ErrInvalidCredentials error.
+	var id int
+	var hashedPass []byte
+	row := m.DB.QueryRow("SELECT id, password FROM users WHERE email = ?", email)
+	err := row.Scan(&id, &hashedPass)
+	if err == sql.ErrNoRows {
+		return 0, models.ErrInvalidCredentials
+	} else if err != nil {
+		return 0, err
+	}
+
+	// Check whether the hashed password and plain text password provided match.
+	// If they don't, we return the ErrInvalidCredentials error.
+	err = bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
+	if err == bcrypt.ErrMismatchedHashAndPassword {
+		return 0, models.ErrInvalidCredentials
+	} else if err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
 
 // Get() method fetches the details for a specific user based on their ID.
