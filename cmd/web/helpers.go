bf8955c895111a948b1355226aefc5c6ecc235d7
Ardian Maulana Effendi <ardian.effendi21@gmail.com>
2023-02-15 21:55:36 +0800

    use Request Context for authentication/authorization This way, we eliminate the extra round trip to database as now user checking is done once in middleware which helps pass the information to the subsequent handlers.

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -7,6 +7,7 @@ import (
 	"runtime/debug"
 	"time"
 
+	"github.com/ardianeffendi/snippetbox/pkg/models"
 	"github.com/justinas/nosurf"
 )
 
@@ -47,10 +48,14 @@ func (app *application) addDefaultData(td *templateData, r *http.Request) *templ
 	return td
 }
 
-// The authenticatedUser method returns the ID of the current user from the
-// session, or zero if the request is from an authenticated user.
-func (app *application) authenticatedUser(r *http.Request) int {
-	return app.session.GetInt(r, "userID")
+// The authenticatedUser returns the User's struct of the current user from
+// context and it returns nil if user is no authenticated and valid user.
+func (app *application) authenticatedUser(r *http.Request) *models.User {
+	user, ok := r.Context().Value(contextKeyUser).(*models.User)
+	if !ok {
+		return nil
+	}
+	return user
 }
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
